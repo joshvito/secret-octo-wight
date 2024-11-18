4e35d115878ced25f7b58567d48c60b7acbb47dc
Joshua Vito <jvito@anthology.com>
2024-11-18 17:18:31 -0500

    adds test api call to devops

--- buildandreleasetask/index.ts
+++ buildandreleasetask/index.ts
@@ -3,7 +3,6 @@ import tl = require('azure-pipelines-task-lib/task');
 import { Buffer } from 'node:buffer';
 
  async function run() {
-    // https://dev.azure.com/campuslabs/CollegiateLink/_git/Pillar.SisIntegration/pullrequest/38751
     const encode = (str: string):string => Buffer.from(':'+ str).toString('base64');
 
     const url = `https://dev.azure.com/${process.env.debug_organization}/${process.env.debug_project}/_apis/git/pullrequests/${process.env.debug_pullRequestId}?api-version=7.1`;
